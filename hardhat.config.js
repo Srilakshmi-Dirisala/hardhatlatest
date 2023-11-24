d6cd5cc56aa212cad70b426c603556ac218df75c
Srilakshmi <dirisalasrilakshmi@gmail.com>
2023-11-24 16:52:25 +0530

    added

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+const INFURA_URL='https://goerli.infura.io/v3/797c36988b6c4e14993ea637860bf5f3'
+const PRIVATE_KEY='354a56c74d1c990d65f213a504bd5c5edbe6a51439182d18c264c056bf3a5878'
+
 module.exports = {
   solidity: "0.8.19",
+  networks:{
+    goerli:{
+      url:INFURA_URL,
+      accounts:[`0x${PRIVATE_KEY}`]
+    }
+  }
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,29 +5,33 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
-
+const fs=require('fs')
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const [deployer]=await hre.ethers.getSigners()
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  const balance=await deployer.provider.getBalance(deployer.address);
+  console.log(`Account balance: ${balance.toString()}`);
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  const Token=await hre.ethers.getContractFactory('Token');
+  const token=await Token.deploy();
+  console.log(`Token address: ${token.target}`);
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  const data = {
+    address: token.target,
+    abiJsonString: JSON.stringify(token.interface),
+    abiString: token.interface
+  };
+  console.log("abistring",data);
 }
+  
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
+main()
+.then(()=>{
+  process.exit(0)
+})
+.catch((error) => {
   console.error(error);
-  process.exitCode = 1;
+  process.exit(1);
 });
